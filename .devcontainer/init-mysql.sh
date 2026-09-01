3b6062dec82cbba5f8d152ce5af0ce9d2281e808
Sérgio Roberto Delfino <srdelfino@gmail.com>
2026-09-01 18:19:46 +0000

    Criação do Projeto de Eventos

--- .devcontainer/init-mysql.sh
+++ .devcontainer/init-mysql.sh
@@ -5,12 +5,9 @@ set -e
 service mysql start
 
 mysql <<EOF
-CREATE DATABASE IF NOT EXISTS eventos CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
 CREATE USER IF NOT EXISTS 'aluno'@'%' IDENTIFIED BY '123@Mudar';
 
-GRANT ALL PRIVILEGES ON eventos.* TO 'aluno'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'aluno'@'%';
 
 FLUSH PRIVILEGES;
-
 EOF
